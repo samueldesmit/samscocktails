dba457315cf966048bbad87030cef26a4adf16a0
samueldesmit <samuel_desmit@hotmail.com>
2024-08-31 10:42:17 +0200

    added footer and some styling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import './Footer.scss';
+
+function Footer() {
+
+  return (
+    <div className='footer'>
+      <div className='footer__inner'>
+        <div className='footer-information-column'>
+          <p>Sam's Cocktails  ©</p>
+          <p>2024</p>
+        </div>
+
+        <div className='footer-information-column'>
+          <p>Powered by</p>
+          <a href='https://www.thecocktaildb.com/' target='_blank'>TheCocktailDB</a>
+        </div>
+        <div className='footer-information-column'>
+          <p>Sepcial thanks to</p>
+          <ul>
+            <li>Rowan Plooij</li>
+            <li>Sam Barnhoorn</li>
+            <li>Nova Eeken</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
